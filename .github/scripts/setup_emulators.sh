fee4d33a7f57f4e4033091135955df68318e0e3c
khammami <khammami@gmail.com>
2024-02-06 12:54:31 +0100

    test

--- .github/scripts/setup_emulators.sh
+++ .github/scripts/setup_emulators.sh
@@ -9,15 +9,15 @@ API_LEVEL=29
 ANDROID_HOME_SDK="/usr/local/lib/android/sdk"
 
 # Check Emulator already Installed
-EMULATOR_COUNT=$($ANDROID_HOME_SDK/tools/emulator -list-avds | wc -l)
+EMULATOR_COUNT=$($ANDROID_HOME_SDK/cmdline-tools/emulator -list-avds | wc -l)
 
 if [[ "$EMULATOR_COUNT" -gt 0 ]]; then 
     echo "Emulator already available"
 else
     # Install Emulator
-    $ANDROID_HOME_SDK/tools/bin/sdkmanager --update
-    $ANDROID_HOME_SDK/tools/bin/sdkmanager --install "emulator"
-    $ANDROID_HOME_SDK/tools/bin/sdkmanager --install "system-images;android-${API_LEVEL};google_apis;x86"
+    $ANDROID_HOME_SDK/cmdline-tools/bin/sdkmanager --update
+    $ANDROID_HOME_SDK/cmdline-tools/bin/sdkmanager --install "emulator"
+    $ANDROID_HOME_SDK/cmdline-tools/bin/sdkmanager --install "system-images;android-${API_LEVEL};google_apis;x86"
     # #echo "no" | ${ANDROID_HOME_SDK}/tools/bin/avdmanager --verbose create avd --force --name "pixel" --device "pixel" --package "system-images;android-${API_LEVEL};google_apis;x86" --tag "google_apis" --abi "x86"
     # $ANDROID_HOME_SDK/emulator/emulator @avd-test -no-window -no-boot-anim -netdelay none -no-snapshot -wipe-data -verbose -show-kernel -no-audio -gpu swiftshader_indirect -no-snapshot &> /tmp/log.txt &
     # adb wait-for-device shell "while [[ -z $(getprop sys.boot_completed) ]]; do sleep 5; done; input keyevent 82"
