97dc53b7d1a75d94680d8fa6f3c0cec256be136a
khammami <khammami@gmail.com>
2024-02-06 11:45:16 +0100

    tests

--- .github/scripts/setup_emulators.sh
+++ .github/scripts/setup_emulators.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # fail if any commands fails
 set -e
 # debug log
@@ -14,11 +14,11 @@ then
     echo "Emulator already available"
 else
     # Install Emulator
-    sudo ${ANDROID_HOME_SDK}/tools/bin/sdkmanager --update
-    ${ANDROID_HOME_SDK}/tools/bin/sdkmanager --install "emulator"
-    ${ANDROID_HOME_SDK}/tools/bin/sdkmanager --install "system-images;android-${API_LEVEL};google_apis;x86"
+    sudo $ANDROID_HOME_SDK/tools/bin/sdkmanager --update
+    $ANDROID_HOME_SDK/tools/bin/sdkmanager --install "emulator"
+    $ANDROID_HOME_SDK/tools/bin/sdkmanager --install "system-images;android-${API_LEVEL};google_apis;x86"
     #echo "no" | ${ANDROID_HOME_SDK}/tools/bin/avdmanager --verbose create avd --force --name "pixel" --device "pixel" --package "system-images;android-${API_LEVEL};google_apis;x86" --tag "google_apis" --abi "x86"
-    ${ANDROID_HOME_SDK}/emulator/emulator @avd-test -no-window -no-boot-anim -netdelay none -no-snapshot -wipe-data -verbose -show-kernel -no-audio -gpu swiftshader_indirect -no-snapshot &> /tmp/log.txt &
+    $ANDROID_HOME_SDK/emulator/emulator @avd-test -no-window -no-boot-anim -netdelay none -no-snapshot -wipe-data -verbose -show-kernel -no-audio -gpu swiftshader_indirect -no-snapshot &> /tmp/log.txt &
     adb wait-for-device shell "while [[ -z $(getprop sys.boot_completed) ]]; do sleep 5; done; input keyevent 82"
     adb shell settings put global window_animation_scale 0.0
     adb shell settings put global transition_animation_scale 0.0
