6031d219ac87e44c1759a35a3ac83a4a12e4cb89
khammami <khammami@gmail.com>
2024-02-08 12:52:50 +0100

    test

--- .github/scripts/install_android_sdk.sh
+++ .github/scripts/install_android_sdk.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#ANDROID_HOME="/home/khammami/Android/Sdk"
 cmdlineToolsPath="$ANDROID_HOME/cmdline-tools"
 
 # Define package names and versions
 BUILD_TOOLS="build-tools;34.0.0"
 PLATFORM_TOOLS="platform-tools"
 
+echo "Grant permission for Android Home Sdk..."
 sudo chown $USER:$USER $ANDROID_HOME -R >/dev/null
 
 #add paths for commandline-tools and platform-tools
@@ -16,16 +16,19 @@ export PATH="$cmdlineToolsPath/latest:$cmdlineToolsPath/latest/bin:$ANDROID_HOME
 export ANDROID_AVD_HOME="$HOME/.android/avd"
 
 # accept all Android SDK licenses
-\yes | sdkmanager --licenses
+echo "Accept all licenses..."
+yes | sdkmanager --licenses >/dev/null
 
-sdkmanager --update
+# Update sdkmanager packages to the latest version
+echo "Updates installed packages in sdkmanager..."
+sdkmanager --update >/dev/null
 
 #sdkmanager --install "cmdline-tools;latest" >/dev/null
 
 # Check if packages are installed
 if ! sdkmanager --list | sed -e '/Available Packages/q' | grep -q "$BUILD_TOOLS"; then
   echo "Build tools $BUILD_TOOLS not found. Installing..."
-  sdkmanager --install "$BUILD_TOOLS" 
+  sdkmanager --install "$BUILD_TOOLS" >/dev/null
   echo "$BUILD_TOOLS has been installed successfully!"
 else
   echo "Build tools $BUILD_TOOLS already installed."
@@ -33,7 +36,7 @@ fi
 
 if ! sdkmanager --list | sed -e '/Available Packages/q' | grep -q "$PLATFORM_TOOLS"; then
   echo "Platform tools not found. Installing..."
-  sdkmanager --install "$PLATFORM_TOOLS" 
+  sdkmanager --install "$PLATFORM_TOOLS" >/dev/null
   echo "Platform tools has been installed successfully!"
 else
   echo "Platform tools already installed."
@@ -42,7 +45,7 @@ fi
 # Check for emulator (more generic approach)
 if ! sdkmanager --list | sed -e '/Available Packages/q' | grep -q "emulator"; then
   echo "Emulator not found. Installing..."
-  sdkmanager --install emulator --channel=0
+  sdkmanager --install emulator --channel=0 >/dev/null
   echo "Emulator has been installed successfully!"
 else
   echo "Emulator already installed."
