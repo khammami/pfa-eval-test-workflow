45558e7301b5d24bbe0bfca07b43de234a71c266
khammami <khammami@gmail.com>
2024-02-08 13:07:35 +0100

    test

--- .github/scripts/install_android_sdk.sh
+++ .github/scripts/install_android_sdk.sh
@@ -5,6 +5,7 @@ cmdlineToolsPath="$ANDROID_HOME/cmdline-tools"
 # Define package names and versions
 BUILD_TOOLS="build-tools;34.0.0"
 PLATFORM_TOOLS="platform-tools"
+SDKMANAGER_LIST_DELIM='/Available Packages/q'
 
 echo "Grant permission for Android Home Sdk..."
 sudo chown $USER:$USER $ANDROID_HOME -R >/dev/null
@@ -17,16 +18,16 @@ export ANDROID_AVD_HOME="$HOME/.android/avd"
 
 # accept all Android SDK licenses
 echo "Accept all licenses..."
-"y" | sdkmanager --licenses >/dev/null
+y | sdkmanager --licenses >/dev/null
 
 # Update sdkmanager packages to the latest version
-echo "Updates installed packages in sdkmanager..."
+echo "Update installed packages in sdkmanager..."
 sdkmanager --update >/dev/null
 
 #sdkmanager --install "cmdline-tools;latest" >/dev/null
 
 # Check if packages are installed
-if ! sdkmanager --list | sed -e '/Available Packages/q' | grep -q "$BUILD_TOOLS"; then
+if ! sdkmanager --list | sed -e "$SDKMANAGER_LIST_DELIM" | grep -q "$BUILD_TOOLS"; then
   echo "Build tools $BUILD_TOOLS not found. Installing..."
   sdkmanager --install "$BUILD_TOOLS" >/dev/null
   echo "$BUILD_TOOLS has been installed successfully!"
@@ -34,7 +35,7 @@ else
   echo "Build tools $BUILD_TOOLS already installed."
 fi
 
-if ! sdkmanager --list | sed -e '/Available Packages/q' | grep -q "$PLATFORM_TOOLS"; then
+if ! sdkmanager --list | sed -e "$SDKMANAGER_LIST_DELIM" | grep -q "$PLATFORM_TOOLS"; then
   echo "Platform tools not found. Installing..."
   sdkmanager --install "$PLATFORM_TOOLS" >/dev/null
   echo "Platform tools has been installed successfully!"
